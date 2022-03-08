b078fb56c2dc6012093aba2b943f69ccc51e1d1f
razikusman <UsmanGit2111@gmail.com>
2022-03-08 15:51:18 +0530

    AspNetCore.Mvc.Testing added

---
+++ sale-API/sale-API.Acceptance.Test/Brokers/sale-APIBroker.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sale_API.Acceptance.Test.Brokers
+{
+    public class sale_APIBroker
+    {
+        public readonly WebApplicationFactory<Startup> { get; set; }
+    }
+}
---
+++ sale-API/sale-API.Acceptance.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace sale_API.Acceptance.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
