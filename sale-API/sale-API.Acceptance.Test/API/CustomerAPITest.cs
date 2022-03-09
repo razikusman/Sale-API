cba2de9f08735cc25f12dd6b9f2f42ddd1d291f5
razikusman <UsmanGit2111@gmail.com>
2022-03-09 11:01:03 +0530

    Acceptance test Post compleeted -  8-3-2022

---
+++ sale-API/sale-API.Acceptance.Test/API/CustomerAPITest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using sale_API.Acceptance.Test.Brokers;
+using sale_API.Acceptance.Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace sale_API.Acceptance.Test.API
+{
+    [Collection(nameof(ApiTestCollection))]
+    public class CustomerAPITest
+    {
+        private readonly sale_ApiBroker sale_ApiBroker;
+
+        public CustomerAPITest(sale_ApiBroker sale_ApiBroker) =>
+            this.sale_ApiBroker = sale_ApiBroker;
+
+        private Customer Createcustomer() =>
+            new Filler<Customer>().Create();
+
+        [Fact]
+        public async Task shouldPostAsync()
+        {
+            //give
+            Customer randomcustomer = Createcustomer();
+            Customer inputcustomer = randomcustomer;
+            Customer expecteedsustomer = inputcustomer;
+
+            //when
+            Customer actualcustomer =
+                await this.sale_ApiBroker.PostCustomerAsync(inputcustomer);
+
+            //then
+            actualcustomer.Should().BeEquivalentTo(expecteedsustomer);
+        }
+
+    }
+}
