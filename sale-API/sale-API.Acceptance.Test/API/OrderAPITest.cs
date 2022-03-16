1907788cc17e2f9184979111be33207a7c422c46
razikusman <UsmanGit2111@gmail.com>
2022-03-16 22:44:28 +0530

    Testing Orders

---
+++ sale-API/sale-API.Acceptance.Test/API/OrderAPITest.cs
@@ -0,0 +1,109 @@
+﻿using FluentAssertions;
+using sale_API.Acceptance.Test.Brokers;
+using sale_API.Acceptance.Test.Models;
+using sale_API.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace sale_API.Acceptance.Test.API
+{
+    [Collection(nameof(ApiTestCollection))]
+    public class OrderAPITest
+    {
+        private readonly sale_ApiBroker sale_ApiBroker;
+
+        public OrderAPITest(sale_ApiBroker sale_ApiBroker) =>
+            this.sale_ApiBroker = sale_ApiBroker;
+
+        private Order Createorder() =>
+            new Filler<Order>().Create();
+        
+        //create - test
+        [Fact]
+        public async Task shouldPostAsync()
+        {
+            //give
+            Order randomorder = Createorder();
+            Order inputorder = randomorder;
+            inputorder.ItemID = 1;
+            inputorder.InvoiceID = 1;
+
+            Order expectedorder =  inputorder;
+
+            //when
+            Order actualorder =
+                await this.sale_ApiBroker.PostOrderAsync(inputorder);
+
+            //then
+            actualorder.Should().BeEquivalentTo(expectedorder);
+
+        }
+
+        //retrieve - test
+       /* [Fact]
+        public async Task shouldGetAsync()
+        {
+            //give
+            Order randomorder = Createorder();
+            Order inputorder = randomorder;
+            
+            int orderID = 5;
+            Order expectedorder =
+                await this.sale_ApiBroker.GetOrderAsync(orderID);
+
+            //when
+            Order actualorder =
+                await this.sale_ApiBroker.PostOrderAsync(inputorder);
+
+            //then
+            actualorder.Should().BeEquivalentTo(expectedorder);
+
+        }*/
+
+        //update - test
+        [Fact]
+        public async Task shouldPutAsync()
+        {
+            int orderID = 4;
+            
+            //give
+            Order updateorder =
+                await this.sale_ApiBroker.GetOrderAsync(orderID);
+
+            updateorder.O_qty = 000 + updateorder.O_qty; //updated
+
+            Order expectedsustomer = updateorder;
+
+            //when
+            Order actualorder =
+                await this.sale_ApiBroker.PutOrderAsync(orderID , updateorder);
+
+            //then
+            actualorder.Should().BeEquivalentTo(expectedsustomer);
+        }
+
+        //delete - test
+        [Fact]
+        public async Task shouldDeleteAsync()
+        {
+            int orderID = 5;
+
+            //given
+            Order actualorder = 
+                await this.sale_ApiBroker.GetOrderAsync(orderID);
+
+            //when
+            Order deletedorder = 
+                await this.sale_ApiBroker.DeleteOrderAsync(orderID);
+
+            //then
+            actualorder.Should().BeEquivalentTo(deletedorder);
+        }
+
+    }
+}
---
+++ sale-API/sale-API.Acceptance.Test/Brokers/slae-ApiBroker.Order.cs
@@ -0,0 +1,30 @@
+﻿using sale_API.Acceptance.Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sale_API.Acceptance.Test.Brokers
+{
+    public partial class sale_ApiBroker
+    {
+        private readonly string OrderRelativeURl = "Orders";
+
+        //create
+        public async ValueTask<Order> PostOrderAsync(Order order) =>
+            await this.apiFactoryClient.PostContentAsync(OrderRelativeURl, order);
+
+        //retrive
+        public async ValueTask<Order> GetOrderAsync(int orderID) =>
+            await this.apiFactoryClient.GetContentAsync<Order>($"{OrderRelativeURl}/{orderID}");
+
+        //update
+        public async ValueTask<Order> PutOrderAsync(int orderID, Order order) =>
+            await this.apiFactoryClient.PutContentAsync<Order>($"{OrderRelativeURl}/{orderID}", order);
+
+        //delete
+        public async ValueTask<Order> DeleteOrderAsync(int orderID) =>
+            await this.apiFactoryClient.DeleteContentAsync<Order>($"{OrderRelativeURl}/{orderID}");
+    }
+}
