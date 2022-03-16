2f30c638ee95ea537b8e927adece15183d98c083
razikusman <UsmanGit2111@gmail.com>
2022-03-16 22:42:46 +0530

    Testing Invoices

---
+++ sale-API/sale-API.Acceptance.Test/API/InvoiceAPITest.cs
@@ -0,0 +1,108 @@
+﻿using FluentAssertions;
+using sale_API.Acceptance.Test.Brokers;
+using sale_API.Acceptance.Test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace sale_API.Acceptance.Test.API
+{
+    [Collection(nameof(ApiTestCollection))]
+    public class InvoiceAPITest
+    {
+        private readonly sale_ApiBroker sale_ApiBroker;
+
+        public InvoiceAPITest(sale_ApiBroker sale_ApiBroker) =>
+            this.sale_ApiBroker = sale_ApiBroker;
+
+        private Invoice Createinvoice() =>
+            new Filler<Invoice>().Create();
+        
+        //create - test
+        [Fact]
+        public async Task shouldPostAsync()
+        {
+            //give
+            Invoice randominvoice = Createinvoice();
+            Invoice inputinvoice = randominvoice;
+            inputinvoice.CustomerID = 1;
+
+            Invoice expectedinvoice = inputinvoice;
+
+            //when
+            Invoice actualinvoice =
+                await this.sale_ApiBroker.PostInvoiceAsync(inputinvoice);
+
+            //then
+            actualinvoice.Should().BeEquivalentTo(expectedinvoice);
+
+        }
+
+        //retrieve - test
+        /*[Fact]
+        public async Task shouldGetAsync()
+        {
+            //give
+            Invoice randominvoice = Createinvoice();
+            Invoice inputinvoice = randominvoice;
+            
+            int invoiceID = 5;
+            Invoice expectedinvoice =
+                await this.sale_ApiBroker.GetInvoiceAsync(invoiceID);
+
+            //when
+            Invoice actualinvoice =
+                await this.sale_ApiBroker.PostInvoiceAsync(inputinvoice);
+
+            //then
+            actualinvoice.Should().BeEquivalentTo(expectedinvoice);
+
+        }*/
+
+        //update - test
+        [Fact]
+        public async Task shouldPutAsync()
+        {
+            int invoiceID = 4;
+            
+            //give
+            Invoice updateinvoice =
+                await this.sale_ApiBroker.GetInvoiceAsync(invoiceID);
+
+            updateinvoice.I_Note = "updated " + updateinvoice.I_Note; //updated
+            updateinvoice.CustomerID = 1;
+
+            Invoice expectedsustomer = updateinvoice;
+
+            //when
+            Invoice actualinvoice =
+                await this.sale_ApiBroker.PutInvoiceAsync(invoiceID , updateinvoice);
+
+            //then
+            actualinvoice.Should().BeEquivalentTo(expectedsustomer);
+        }
+
+        //delete - test
+        [Fact]
+        public async Task shouldDeleteAsync()
+        {
+            int invoiceID = 5;
+
+            //given
+            Invoice actualinvoice = 
+                await this.sale_ApiBroker.GetInvoiceAsync(invoiceID);
+
+            //when
+            Invoice deletedinvoice = 
+                await this.sale_ApiBroker.DeleteInvoiceAsync(invoiceID);
+
+            //then
+            actualinvoice.Should().BeEquivalentTo(deletedinvoice);
+        }
+
+    }
+}
--- sale-API/sale-API.Acceptance.Test/Models/Invoice.cs
+++ sale-API/sale-API.Acceptance.Test/Models/Invoice.cs
@@ -7,7 +7,7 @@ namespace sale_API.Acceptance.Test.Models
 {
     public class Invoice
     {
-        public int InvoiceID { get; set; }
+       // public int InvoiceID { get; set; }
         public int I_Num { get; set; }
         public DateTime I_Date { get; set; }
         public int I_RefNum { get; set; }
@@ -20,6 +20,6 @@ namespace sale_API.Acceptance.Test.Models
 
 
         //orders
-        public virtual List<Order> Orders { get; set; }
+        //public virtual List<Order> Orders { get; set; }
     }
 }
